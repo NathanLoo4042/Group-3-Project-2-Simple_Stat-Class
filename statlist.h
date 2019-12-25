9aab0f794393c780f88556caa0939f7acdf662aa
zkimccsf <42948370+zkimccsf@users.noreply.github.com>
2019-12-24 21:01:53 -0800

    Add files via upload
    Fixed Mode method

--- statlist.h
+++ statlist.h
@@ -52,13 +52,13 @@ template <typename E> class statlist{
     }
 
     void updateMode() {
-      int a = 0;
+      int modeindex = 0;
       for(int i = 0; i < length; i++) {
-        if(a < data[i].second) {
-          a = i;
+        if(data[modeindex].second < data[i].second) {
+          modeindex = i;
         }
       }
-      mode = data[a].first;
+      mode = data[modeindex].first;
     }
 
     void updateMedian() {
